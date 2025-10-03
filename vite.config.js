034a82b7db1de6e0df7f941c903fbac3e0394320
Okezy <nwokeokechukwu1234@gmail.com>
2025-10-03 17:13:59 +0100

    feat: Added gh-pages for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  
+  // ADD THIS BASE PATH FOR GITHUB PAGES
+  // The value must be the name of your GitHub repository, surrounded by slashes.
+  base: "/my-church-website/", 
 })
